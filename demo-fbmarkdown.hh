0e1fa09b74e0096f08be3c8b9da578c85089b6ef
yanicky <33847588+yanicky@users.noreply.github.com>
2019-08-31 18:50:22 -0400

    Create demo-fbmarkdown.hh

---
+++ demo-fbmarkdown.hh
@@ -0,0 +1,18 @@
+<?hh
+require __DIR__ . '/vendor/hh_autoload.hh';
+use namespace Facebook\Markdown;
+
+function render(string $markdown): string {
+  $ast = Markdown\parse(new Markdown\ParserContext(), $markdown);
+
+  $html = (new Markdown\HTMLRenderer(
+    new Markdown\RenderContext()
+  ))->render($ast);
+
+  return $html;
+}
+
+$markdown = file_get_contents('https://raw.githubusercontent.com/yanicky/KaT/master/README.md');
+
+$html = render($markdown);
+echo $html;
