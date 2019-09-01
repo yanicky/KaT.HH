77e5deeb30f5dafc4d5c974339e0a365e2b10d9a
yanicky <33847588+yanicky@users.noreply.github.com>
2019-08-31 20:43:17 -0400

    Update index.hh

--- index.hh
+++ index.hh
@@ -7,37 +7,9 @@
 error_reporting(0);
 
 require __DIR__ . '/vendor/hh_autoload.hh';
-use namespace Facebook\Markdown;
-
-function render(string $markdown): string {
-  $ast = Markdown\parse(new Markdown\ParserContext(), $markdown);
-
-  $html = (new Markdown\HTMLRenderer(
-    new Markdown\RenderContext()
-  ))->render($ast);
-
-  return $html;
-}
+include("src/minibase.hh");
 
 
-function jsonCurl($myurl, $mymethod, $mypayload) 
-        {
-	//create a new cURL resource
-	$ch = curl_init($myurl);
-	curl_setopt($ch, CURLOPT_POST, TRUE);
-	//attach encoded JSON string to the POST fields
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $mypayload);
-	//set the content type to application/json
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-	//return response instead of outputting
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	//execute the POST request
-	$res = curl_exec($ch);
-	//close cURL resource
-	curl_close($ch);
-	//return the result
-	return $res;
-	}
 
 // To be used with php-cli in console (ie: php index.php --wallet=yourwalletaddresshere)
 foreach( $argv as $argument ) {
